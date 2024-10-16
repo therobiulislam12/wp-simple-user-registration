3c9707518dfc95dd8258e4ffadf79aa860c0aae9
Robiul Islam <therobiulislam12@gmail.com>
2024-10-16 13:31:50 +0600

    Menu page added on plugin.php page

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -1,26 +1,76 @@
-<?php 
+<?php
 
 namespace UserRegistration\Admin;
 
-class Menu{
-    public function __construct(){
-        add_action('admin_menu', array($this, 'sur_add_admin_page'));
+/**
+ * Handles the admin menu functionality for the plugin.
+ *
+ * This class registers a custom admin page in the WordPress dashboard
+ * and adds a settings link to the plugin's action links.
+ *
+ * @since 1.0.0
+ */
+class Menu {
+
+    /**
+     * Initializes the admin menu and settings actions.
+     *
+     * Menu constructor
+     */
+    public function __construct() {
+
+        // admin menu register
+        add_action( 'admin_menu', array( $this, 'sur_add_admin_page' ) );
+
+        // settings action add
+        add_filter( 'plugin_action_links_' . plugin_basename( RI_USER_REGISTRATION_FILE ), [$this, 'sur_plugin_page_options_add'] );
     }
 
-    public function sur_add_admin_page(){
+    /**
+     * Adds the "User Registration" page to the WordPress admin dashboard.
+     * This page allows managing the settings for the Simple User Registration form.
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function sur_add_admin_page() {
+
         add_menu_page(
-            __('User Registration', 'ri-simple-user-registration'),
-            __('User Registration', 'ri-simple-user-registration'),
+            __( 'User Registration', 'ri-simple-user-registration' ),
+            __( 'User Registration', 'ri-simple-user-registration' ),
             'manage_options',
             'ri-simple-user-registration',
-            array($this, 'sur_user_registration_callback'),
+            array( $this, 'sur_user_registration_callback' ),
             'dashicons-groups',
             2
         );
     }
 
-
-    public function sur_user_registration_callback(){
+    /**
+     * Renders the content of the User Registration admin page.
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function sur_user_registration_callback() {
         echo '<div class="wrap"><h1>I am Working</h1></div>';
+    }
+
+    /**
+     * Adds a "Settings" link to the plugin's action links on the plugins page.
+     *
+     * @param array $links The current plugin action links.
+     * @return array Modified plugin action links with the "Settings" link added.
+     *
+     * @since 1.0.0
+     */
+    public function sur_plugin_page_options_add( $links ) {
+        $settings_links = array(
+            sprintf( '<a href="%1$s">%2$s</a>', admin_url( 'admin.php?page=ri-simple-user-registration' ), __( 'Settings', 'ri-simple-user-registration' ) ),
+        );
+        $links = array_merge( $settings_links, $links );
+        return $links;
     }
 }
