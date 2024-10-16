7a9008197f46bc572bf4dc583face8eb1dd16c7d
Robiul Islam <therobiulislam12@gmail.com>
2024-10-16 15:02:25 +0600

    Include vendor directory

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'therobiulislam12/wp-user-registration',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b907561798c3fa3a5748f539d661b865f9d13cb5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'therobiulislam12/wp-user-registration' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b907561798c3fa3a5748f539d661b865f9d13cb5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
